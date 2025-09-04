2126d43a1167b846f1633083e56d4389fa9a4457
xZeroW <victordmartins@protonmail.com>
2025-09-04 00:06:04 -0300

    b1011

--- Rust/b1011/src/main.rs
+++ Rust/b1011/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    const PI: f64 = 3.14159;
+    let mut R = String::new();
+
+    io::stdin().read_line(&mut R).unwrap();
+
+    let R: f64 = R.trim().parse().unwrap();
+
+    println!("VOLUME = {:.3}", (4.0 / 3.0) * PI * (R * R * R))
 }
